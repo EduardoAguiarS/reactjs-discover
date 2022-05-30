d48e3e05b9d6342e45f00994cfd81a5d7bd21585
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-05-30 13:16:30 -0300

    handle function and states

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,54 @@
+import {useState} from "react"
 import "./styles.scss";
 import {Card} from "../../components/Card";
 
 export const Home = () => {
+    const [name, setName] = useState("");
+    const [students, setStudents] = useState([]);
+
+    const handlePerson = () => {
+        if (name.trim() === "") return;
+        const newStudent = {
+            id: students.length + 1,
+            name: name,
+            time: new Date().toLocaleTimeString("pt-br", {
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+            })
+        };
+
+        setStudents([...students, newStudent]);
+    }
+
     return (
         <div className={"home-container"}>
-            <h1>Lista de Presença</h1>
-            <input type="text" placeholder={"Digite seu nome"}/>
-            <button type={"button"}>Adicionar</button>
-            <Card name={"Eduardo Aguiar"} time={"10:10:32"}/>
-            <Card name={"Eduardo Aguiar"} time={"10:10:32"}/>
-            <Card name={"Eduardo Aguiar"} time={"10:10:32"}/>
+            {/* Header */}
+            <header>
+                <h1>Lista de Presença</h1>
+            </header>
+
+            {/* Form */}
+            <input
+                type="text"
+                placeholder={"Digite seu nome"}
+                onChange={e => setName(e.target.value)}
+            />
+            <button
+                type={"button"}
+                onClick={handlePerson}
+            >
+                Adicionar
+            </button>
+
+            {/* Card */}
+            {students.map(student => (
+                <Card
+                    name={student.name}
+                    time={student.time}
+                    key={student.id}
+                />
+            ))}
         </div>
     );
 };
